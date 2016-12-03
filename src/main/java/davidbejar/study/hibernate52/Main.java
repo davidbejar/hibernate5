56172a0add60693ce4593821a4a40126642755c3
davidbejar <david.bejar@gmail.com>
2016-12-03 18:13:29 +0700

    Create Main dummy class

---
+++ src/main/java/davidbejar/study/hibernate52/Main.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package davidbejar.study.hibernate52;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * @author davidbejar
+ *
+ */
+@EnableAutoConfiguration
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+
+	}
+
+}
